1d298094ba69153dbaa0b9a56160e5de7d23eb6d
Wojtek <szypelt@seo-partner.pl>
2019-08-23 14:49:25 +0200

    Utworzenie szablonu

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -8,11 +8,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="main")
      */
     public function main()
     {
         return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/write-blog", name="write_blog")
+     */
+    public function writeBlog()
+    {
+        return $this->render('front/write-blog.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/registry", name="registry")
+     */
+    public function registry()
+    {
+        return $this->render('front/registry.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('front/login.html.twig', [
             'controller_name' => 'FrontController',
         ]);
     }
